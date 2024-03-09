bde874a3d47079cb11e0acd65be346a63a5b0bd1
seulgijung <seulgi.jung1109@gmail.com>
2024-03-08 23:53:44 -0500

    publish

--- config.js
+++ config.js
@@ -7,21 +7,20 @@ var config = {
     //Read more about available projections here
     //https://docs.mapbox.com/mapbox-gl-js/example/projections/
     inset: false,
-    theme: 'light',
+    // theme: 'light',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
     title: 'Is North Korea Preparing for a War?',
-    subtitle: 'A descriptive and interesting subtitle to draw in the reader',
+    // subtitle: 'A descriptive and interesting subtitle to draw in the reader',
     byline: 'By Seulgi Jung',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     introParagraphs: [
-        "January was not a peaceful start of 2024 for the Koreas. On January 3, Kim Yo-jong, a member of the State Affairs Commission of North Korea and a sister of Kim Jong-un, supreme leader of North Korea, announced a statement densely filled with mockery and sarcasm toward the South Korean government (링크삽입). Two days later, on January 5, the North fired artillery at sea against the South militar, forcing residents to move to shelters(링삽), followed by a test-firing of the country's first solid-fuel intermediate-range ballistic missile on January14. It immediately drew condemnation from the U.S. for violation of multiple United Nations Security Council resolutions.(링크삽입)",
-        "But something more menacing dropped later.",
-        "On January 16, Pyongyang laid out its plan to abolish a decades-long formal agreement on peaceful reunification with the South. In a cabinet meeting of the Supreme People's Assembly, Kim Jong-un made an order  to redefine the South as the number one enemy state and specify annexation of South Korea in case of war in the constitution, state media said.",
-        "Is North Korea preparing for war?",
-        "Some experts say yes. According to an article for the U.S.-based website 38 North by two veteran analysts of North Korea — the former State Department official Robert L. Carlin and the nuclear scientist Siegfried S. Hecker — Mr. Kim was done with mere threats. “Kim Jong-un has made a strategic decision to go to war,” they wrote.",
-        "But so far, U.S. agencies seem to view it with a more careful manner. No concrete signs have been detected that the DPRK is gearing up for combat or a major war, the New York Times reported citing interviews with American officials on the condition of anonymity. One official said in an interview with the Times that North Korea will not have been sending large numbers of arms, including sets of artillery rounds and missiles, even proved to be the newest, to Russia if it’s planning a prolonged conflict.",
-        "Aerial photographs, in a UK’s unpublished report submitted to a panel of UN experts as part of an attempt to trigger an official investigation into arms deals in violation of international sanctions, captured several Russian ships loading containers at North Korea’s Najin Port, which was also confirmed by the U.S. government. The accusation has been officially denied by both North Korea and Russia.",
+        "January was not a peaceful start of 2024 for the two Koreas. On January 3, Kim Yo-jong, a member of the North Korean State Affairs Commission and  sister to Kim Jong-un, supreme leader of North Korea, announced a statement densely filled with mockery and sarcasm toward the South Korean government. Two days later, the North fired 200 rounds of artillery into the sea near two South Korean islands, forcing residents to flee to bomb shelters. Just nine days later, the North fired its first test of a  solid-fuel intermediate-range ballistic missile, drawing immediate condemnation from the U.S. for violating multiple U.N. Security Council resolutions.",
+        "Despite all that, the biggest bombshell was yet to come.",
+        "On January 16, Pyongyang announced plans to abolish a decades-long formal agreement on peaceful reunification with the South. In a cabinet meeting of the Supreme People's Assembly, Kim Jong-un called for a constitutional amendment to define South Korea as the North’s number one enemy and to specify annexation of South Korea in case of war.",
+        "Is North Korea preparing for such a war?",
+        "Some experts say yes. According to two veteran analysts of North Korea,e former State Department official Robert L. Carlin and nuclear scientist Siegfried S. Hecker, Mr. Kim’s provocations are no mere threats. “Kim Jong-un has made a strategic decision to go to war,” they say.",
+        "So far, U.S. agencies are taking a more measured view. The New York Times, citing interviews with anonymous American officials, reports that no concrete signs have been detected that the DPRK is gearing up for combat or a major war. One official cited by the Times said North Korea would not be making large arms shipments to Russia,, including new sets of artillery rounds and missiles, if it were planning a prolonged conflict.",
     ],
     chapters: [
         {
@@ -30,7 +29,7 @@ var config = {
             hidden: false,
             title: 'But here is an evidence',
             image: './path/to/image/source.png',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            description: 'So far, U.S. agencies are taking a more measured view. The New York Times, citing interviews with anonymous American officials, reports that no concrete signs have been detected that the DPRK is gearing up for combat or a major war. One official cited by the Times said North Korea would not be making large arms shipments to Russia, including new sets of artillery rounds and missiles, if it were planning a prolonged conflict.',
             location: {
                 center: [125.6, 39.7],
                 zoom: 6,
@@ -60,7 +59,7 @@ var config = {
             hidden: false,
             title: 'Najin Turn On',
             image: './path/to/image/source.png',
-            description: 'Copy these sections to add to your story.',
+            description: "Despite denials from both Russia and North Korea, an unpublished UK report submitted to the UN offers photographic evidence of the arms packages leaving the North. Aerial photos show Russian ships loading containers at North Korea's Najin Port, a claim confirmed by the US government. Such arms shipments to Russia violate international sanctions.",
             location: {
                 center: [130.10, 42.24],
                 zoom: 9,
@@ -91,12 +90,12 @@ var config = {
             ]
         },
         {
-            id: 'third-identifier',
+            id: 'step-three',
             alignment: 'left',
             hidden: false,
             title: 'Ryonpho Turn On',
             image: './path/to/image/source.png',
-            description: 'Copy these sections to add to your story.',
+            description: 'Those aren’t the only aerial photographs to suggest the North might not be pursuing war. Suh Jae-jung, a professor in the Department of Politics and International Studies at International Christian University, says that the North has transformed  an air base in Ryopho into a greenhouse site. “A leader planning for a major military operation would not have chosen such transition,” he added. “The North’s hostile gesture may come out of escalating tension and we should be wary of blowing it out of proportion.',
             location: {
                 center: [127.09, 39.81],
                 zoom: 8,
@@ -122,6 +121,12 @@ var config = {
         },
     ],
     outroParagraphs: [
-        "어쩌라고"
+        "So perhaps full-scale war is not imminent. Surely, Pyongyang’s recent hardline posturing  is consistent  with a long held pattern of provocations. Still, South Korea is well advised to remain alert and even expect minor skirmishes ahead, possibly in the Demilitarized Zone or the buffer zone in the West sea. U.S. officials warn that Mr. Kim could conduct some form of lethal military action against South Korea in the coming months after shifting to a policy of open hostility, and it would be more dreadful than the 2010 shelling against Yeonpyeong Island, which left four dead and scores more people injured.",
+        "Peace activists argue that in order to draw North Korea back to talks, the first step is a halt to all military exercises that strain relations,such as the ROK-US joint military drills. Jonghun Kapsong Kim, director at the Korean American Peace Fund, said it’s absurd to demand the North cease all military operations that pose a threat to the Korean peninsula at the same time as the R.O.K carries out some of the world’s largest joint military drills with the with the U.S. multiple times every year.",
+        "The current trend towards escalation includes ongoing verbal provocation in the South as well, namely from the government of Yoon Suk-yeol, South Korea’s conservative president. McCarthyism remains rampant as a powerful political weapon dating back to the Korean War, and the nation’s conservative party has traditionally benefited from security issues with the DPRK, especially around election season. South Korea expects to hold National Assembly elections in April.",
+        "Since Mr. Yoon took office in 2022, diplomatic policy on the DPRK has grown extremely belligerent, aligning with the US’s “no-tolerance” policy that emphasizes  sanctions and isolation. Last October, Defense Minister Shin Won-sik made headlines announcing a new offensive military posture if the North provoves “PISU: Punish Immediately, Strongly, and Until the end (즉각, 강력히, 끝까지 응징)",
+        "For many Koreans like Kang Sung-ae, whose husband died in the bombardment of Yeonpyeong Island, all the posturing obscures tragedy.",
+        'When my husband died, I wondered if our military should have responded more strongly,” shesaid in an interview 2 years after the incident.. “But politicians seem to be all similar. It seems like they only listen to us during election season and that’s it once they get elected."',
+        'Mr. Kim was working on the construction of a new residence for the Yeonpyeong Island Marine Corps unit on the day when artillery shells rained down.Ms. Kang knows very well her husband’s death will be forgotten from minds of people as time passes. "It is fortunate that stability has been restored to Yeonpyeong Island, but those of us who suddenly lost our loved one are still living in tears.”',
     ]
 };
